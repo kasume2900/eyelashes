9e6bd28ef0dc4b8a43d1e4a7826b49a359053a3e
kasume2900 <kasume2900@gmail.com>
2023-02-21 08:02:19 +0300

    update

--- src/components/Slaider/Slaider.tsx
+++ src/components/Slaider/Slaider.tsx
@@ -2,16 +2,31 @@
 import s from './Slaider.module.scss'
 import {slaides} from '../../utils/slides'
 import Carousel from '../Carousel/Carousel'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+//import 'swiper/css'
+import 'swiper/swiper-bundle.min.css'
+
 
 export default function Slaider() {
 
   
   return (
     <div className={s.wrap}>
-      <Carousel autoSlade={false} autoSladeInterval={3000}>
-        {slaides.map(el => <img key={el.id} className={s.img} src={el.img} alt="image" />)}
-      </Carousel>
-      
+      <Swiper
+    spaceBetween={50}
+    slidesPerView={1}
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => console.log(swiper)}
+    navigation
+    pagination={{ clickable: true , dynamicBullets : true }}
+    speed={800}
+  >
+    {slaides.map(el => <SwiperSlide  key={el.id}>
+      <img className={s.img} src={el.img} alt="image" />
+    </SwiperSlide>)}
+  </Swiper>
     </div>
   )
 }
