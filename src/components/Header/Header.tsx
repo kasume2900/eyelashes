79d02f0d9221377184a94cc83b8d9f41c38e585a
kasume2900 <kasume2900@gmail.com>
2023-03-08 08:46:21 +0300

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Layout from '../../Layout/Layout'
 import logo from '../../assets/logo.jpg'
+import logo1 from '../../assets/1.svg'
+import logo2 from '../../assets/2.svg'
 import s from './Header.module.scss'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineClose } from 'react-icons/ai'
@@ -50,7 +52,7 @@ export default function Header({tabSelected} :IHeaderProps) {
       <Layout>
         <div className={s.row}>
           <div className={s.logo}>
-            <img src={logo} alt="logo" />
+            <img src={theme === 'light' ? logo1 : logo2} alt="logo" />
           </div>
           <div className={s.body}>
           <div onClick={changeTheme} className={s.icon}>{theme === 'dark' ? <BsMoonStarsFill /> : <ImSun />}</div>
