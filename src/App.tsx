9b32fc7290694d267c21652cea056c2b3cd08d37
kasume2900 <kasume2900@gmail.com>
2023-03-10 09:20:00 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Nav from "./components/Nav/Nav"
 import Popap from "./components/Popap/Popap"
 import Tabs from "./components/Tabs/Tabs"
 import Layout from "./Layout/Layout"
-import {tabs} from './utils/tabs'
-import {useState } from 'react'
+import { tabs } from './utils/tabs'
+import { useState } from 'react'
 import Slaider from "./components/Slaider/Slaider"
 import Concept from "./components/Concept/Concept"
 import Questions from "./components/Questions/Questions"
@@ -14,30 +14,32 @@ import Footer from "./components/Footer/Footer"
 
 function App() {
 
-  const [active,setActive] = useState(0)
+  const [active, setActive] = useState(0)
 
-  const tabSelected = (index : number) => {
+  const tabSelected = (index: number) => {
     setActive(index)
   }
 
-  
+
 
   return (
-   <> 
-     <Header tabSelected={tabSelected}/>
-     <Layout>
-      <Nav tabSelected={tabSelected} />
-      <Popap tabs={tabs} active={active} tabSelected={tabSelected} />
-      <Tabs active={active} tabSelected={tabSelected} />
-      <p id="about" className='pt-6 text-2xl text-center font-exo font-bold'>Мои работы</p>
-     </Layout>
+    <>
+      <Header tabSelected={tabSelected} />
+      <div className='wrapper'>
+        <Layout>
+          <Nav tabSelected={tabSelected} />
+          <Popap tabs={tabs} active={active} tabSelected={tabSelected} />
+          <Tabs active={active} tabSelected={tabSelected} />
+          <p id="about" className='pt-6 text-2xl text-center font-exo font-bold'>Мои работы</p>
+        </Layout>
+      </div>
       <Slaider />
       <Layout>
       </Layout>
-        <Concept />
-        <Questions />
+      <Concept />
+      <Questions />
       <Footer />
-   </>
+    </>
   )
 }
 
