27d34c6bb9191ec7e51f1d4f991e64b07fce446b
kasume2900 <kasume2900@gmail.com>
2023-03-13 11:41:21 +0300

    update

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import s from './Card.module.scss'
+import lent from '../../assets/lent.png'
+
 
 interface ICardProps {
   img : string
   title : string
   price : string
   desc : string
+  xit? : boolean 
 }
 
 
-export default function Card({title,price,desc,img} :ICardProps) {
+export default function Card({title,price,desc,img,xit} :ICardProps) {
   return (
     <div className={s.wrap}>
       <div className={s.image}>
         <img src={img} alt="img" />
       </div>
+      {xit && <img className={s.lent} src={lent} alt="lent" />}
+      {xit && <div className={s.xit}>хит</div>}
       <p className={s.title}>{title}</p>
       <p className={s.price}>{price}</p>
       <p className={s.description}>{desc}</p>
